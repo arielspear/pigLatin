cf825c5443a4239dd7752df184099a8c67b4cb5e
Guest User <student@epicodus.com>
2015-09-15 11:39:06 -0700

    Method to shift first letter

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 var arrayMaker = function(input) {
-  var inputArray = input.toLowerCase().split('');
+  var inputArray = input.split('');
   return inputArray;
+}
+
+var shifter = function(input) {
+  var inputArray = arrayMaker(input);
+
+  var firstLetter = inputArray.shift();
+  var newArray = inputArray.concat(firstLetter);
+  var result = newArray.join("");
+  return result;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,9 @@ describe('arrayMaker', function() {
       expect(arrayMaker("kitten")).to.eql(['k', 'i', 't', 't', 'e', 'n']);
   })
 });
+
+describe('shifter', function() {
+  it("it shifts the first consonants to the end", function() {
+      expect(shifter("Venus")).to.equal("enusV");
+  })
+});
